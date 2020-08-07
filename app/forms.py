5c342b6a2dbe0eaee2067b62685e8ed3af5b3f9d
Angelica <Angelica137@users.noreply.github.com>
2020-08-07 18:32:58 +0100

    add edit profile view

--- app/forms.py
+++ app/forms.py
@@ -31,5 +31,5 @@ class RegistrationForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[length(min=0, max=140)])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
