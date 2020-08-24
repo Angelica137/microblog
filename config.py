3494b13f7113ea9472525f1ec0baf9da113eafe2
Angelica <Angelica137@users.noreply.github.com>
2020-08-24 17:40:26 +0100

    try to refactor to blueprints

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') #os.environ.get('SQLALCHEMY_DATABASE_URI') or 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Debugging and email support
